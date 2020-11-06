f29d493bd4224a31200ca907a0c65082d3abe84f
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-11-06 10:45:38 +0100

    update

--- LEDWall/src/Applications/Sokoban/Level.h
+++ LEDWall/src/Applications/Sokoban/Level.h
@@ -1,8 +1,10 @@
 class Level{
-    private:
-        Level(){
-            
-        }
     public:
+        Level(){
 
+        }
+    private:
+        Player* player;
+        byte boxsize=0x02;//die Kisten können verschieden groß sein 2*2 oder 1*1 oder vieleicht auch 3*3 boxsize ist meistens auch die stepsize
+        byte stepsize=boxsize;
 };
--- LEDWall/src/Applications/Sokoban/Sokoban.h
+++ LEDWall/src/Applications/Sokoban/Sokoban.h
@@ -12,10 +12,7 @@ void onDataReceive(String data,SystemInterface* systeminterface);
 String getName();
 void onStop(SystemInterface* systeminterface);
 private:
-Player* player;
 Level* level;
-byte boxsize=0x02;//die Kisten können verschieden groß sein 2*2 oder 1*1 oder vieleicht auch 3*3 boxsize ist meistens auch die stepsize
-byte stepsize=boxsize;
-byte actGame[10*15];
+
 
 };
