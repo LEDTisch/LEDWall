ed28ec79b7a694a4c075a4f59eeb7327daa8c619
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-12-12 15:57:27 +0100

    Neopixel ansteuerung geht

--- LEDWall2.0/src/main/java/Main.java
+++ LEDWall2.0/src/main/java/Main.java
@@ -8,9 +8,11 @@ public class Main {
     public static Neopixel neo = new Neopixel(150);
     static SocketController sc = new SocketController();
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         neo.begin();
-
+        //neo.clear();
+        neo.setPixelColor(1,255,255,255);
+        neo.show();
         sc.initSocket();
 
     }
--- LEDWall2.0/src/main/java/neopixeldevice/Neopixel.java
+++ LEDWall2.0/src/main/java/neopixeldevice/Neopixel.java
@@ -1,42 +1,52 @@
 package neopixeldevice;
 
+import com.fazecast.jSerialComm.SerialPort;
+
 import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 public class Neopixel {
+    public SerialPort device;
     public int numpixels=0;
-    private long buffer[];
-    private OutputStream device;
+    private byte buffer[];
+    private byte sendbuffer[];
     public Neopixel(int numpixels){
         this.numpixels=numpixels;
-        buffer=new long[this.numpixels];
+        buffer=new byte[this.numpixels*3];
+        sendbuffer=new byte[4 + this.numpixels*3 + 1];
+        sendbuffer[0]=(byte)0xC9;
+        sendbuffer[1]=(byte)0xDA;
+        byte high = (byte)(this.numpixels*3 >> 8);
+        sendbuffer[2] = high;
+        byte low = (byte)(this.numpixels*3);
+        sendbuffer[3] = low;
+        sendbuffer[sendbuffer.length-1]=(byte)0x36;
     }
-    public void begin() throws FileNotFoundException {
-        File device=new File("/dev/ttyACM0");
-        if(device.exists()) {
-            this.device = new FileOutputStream(device);
-            System.out.println("Connected");
-        }
+    public void begin() throws FileNotFoundException, InterruptedException {
+        device = SerialPort.getCommPort("ttyACM0");
+        device.setBaudRate(112500);
+        device.openPort();
+        TimeUnit.MILLISECONDS.sleep(5000);
+
+        System.out.println("Connected");
 
     }
     public void show(){
-        uint8_t *outputPtr = this->frame.dataPtr;
-        for(int i=0;i<numpixels*3;i=i+3){
-            outputPtr[i+0]=buffer[i/3]>>16;
-            outputPtr[i+1]=(buffer[i/3] & 0xFF00)>>8;
-            outputPtr[i+2]=(buffer[i/3] & 0xFFF0);
+        System.arraycopy(this.buffer,0,sendbuffer,4,this.numpixels);
+        device.writeBytes(sendbuffer,sendbuffer.length);
 
-        }
-
-        this.device.write();
     }
 
-    public void setPixelColor(int n, byte r, byte g, byte b){
+    public void setPixelColor(int n, int r, int g, int b){
         if (n > this.numpixels) return;
         if (r > 255 || r < 0) return;
         if (g > 255 || g < 0) return;
         if (b > 255 || b < 0) return;
 
-        this.buffer[n] = (r << 16 | g << 8 | b);
+        this.buffer[n*3] = (byte)r;
+        this.buffer[n*3+1] = (byte)g;
+        this.buffer[n*3+2] = (byte)b;
+
     }
     public void setBrightness(float brightness){
 
