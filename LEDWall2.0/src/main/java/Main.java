e56e217b25445f5b2dcedc15fab6e4a0d743eabe
Felixpc <46627355+Felixpc@users.noreply.github.com>
2020-12-12 17:03:24 +0100

    update

--- LEDWall2.0/src/main/java/Main.java
+++ LEDWall2.0/src/main/java/Main.java
@@ -1,18 +1,20 @@
 
+import device.neopixeldevice.LEDTisch;
 import device.neopixeldevice.Neopixel;
 
 import java.io.FileNotFoundException;
 
 public class Main {
     public static Neopixel neo = new Neopixel(150);
+    public static LEDTisch table=new LEDTisch(10, 15,1);
     public static SocketController sc = new SocketController();
     public static ApplicationManager am = new ApplicationManager();
 
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
-        neo.begin();
-        neo.clear();
-        neo.setPixelColor(1,255,255,255);
-        neo.show();
-        sc.initSocket();
+        table.init(neo);
+        table.clear();
+        table.setColor(255,0,0);
+        table.drawPixel(0,0);
+        table.show();
     }
 }
--- RPINeopixel/src/utils/Device/neopixeldevice/LEDTisch/LEDTisch.h
+++ RPINeopixel/src/utils/Device/neopixeldevice/LEDTisch/LEDTisch.h
@@ -23,20 +23,20 @@ private:
     Neopixel pixels =  Neopixel(150);
 
 public:
-    LEDTisch(int _x,int _y,int _d);
-    int koordinatensystem(int x,int y);
-    int drawkoordinatensystem(int x,int y);
+    LEDTisch(int _x,int _y,int _d);//da
+    int koordinatensystem(int x,int y);//da
+    int drawkoordinatensystem(int x,int y);//da
     int drawkoordinatensystem(int x,int y,uint32_t color);
     void zahl(int x_zahl, int y_zahl, int zahl);
-    void show();
-    void drehung_set(int d);
+    void show();//da
+    void drehung_set(int d);//da
   //  void buchstaben(int x_wort, int y_wort, char wort[], boolean s, int grenzl, int grenzr, long takt);
-    void setcolor(int r,int g,int b);
-    void clear();
+    void setcolor(int r,int g,int b);//da
+    void clear();//da
     void kreis(int x,int y,int r,int u);
     void rect(int x,int y,int w,int h);
     void setRotation(int r);
-    void init();
+    void init();//da
     void gerade(float x1,float y1,float x2,float y2,float g);
     void drawImage(uint32_t image[],int w,int h);
     void setBrightness(float brightness);
